2d2d2751e0959e1aff667cf89595f8241d08bbd2
Tyler Burton <btburton42@gmail.com>
2015-01-24 17:01:58 -0800

    Fixes some pipeline issues created by reordering the gulp workflow

--- src/scripts/event.js
+++ src/scripts/event.js
@@ -1,9 +1,8 @@
-console.log ('fresh');
 var getPageDetails;
 
 getPageDetails = function(callback) {
   chrome.tabs.executeScript(null, {
-    file: "dist/scripts/contentScript.js"
+    file: "dist/scripts/contentscript.js"
   });
   chrome.runtime.onMessage.addListener(function(message) {
     console.log(message);
